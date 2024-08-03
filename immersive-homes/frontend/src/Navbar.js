6807c8a8d8aa4e6c31f8bc0611882feb54fa297a
Pranav Chaudhari <whitespider080067@gmail.com>
2024-08-04 00:27:36 +0530

    The navbar is responsive now.

--- immersive-homes/frontend/src/Navbar.js
+++ immersive-homes/frontend/src/Navbar.js
@@ -1,5 +1,3 @@
-// Filename - Navbar.js
-
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -7,38 +5,38 @@ import "./Navbar.css";
 function Navbar() {
     return (
         <header>
-        <div class="container-fluid">
-            <nav class="navbar navbar-expand-lg bg-custom">
-                <Link className="navbar-brand ms-2" to="/">Immersive Homes</Link>
-                <button class="navbar-toggler me-4" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <Link class="nav-link text-bold" to="/">Home</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link class="nav-link text-bold" to="/explore">Explore</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link class="nav-link text-bold" to="/upload">Upload</Link>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item me-4">
-                            <Link class="nav-link" to="/login">Login</Link>
-                        </li>
-                        {/* <li class="nav-item me-4">
-                            <Link class="nav-link" to="/register">Register</Link>
-                        </li> */}
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </header>
+            <div className="container-fluid">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container-fluid">
+                        <Link className="navbar-brand" to="/">Immersive Homes</Link>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <Link className="nav-link text-bold active" aria-current="page" to="/">Home</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link className="nav-link text-bold" to="/explore">Explore</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link text-bold" to="/upload">Upload</Link>
+                                </li>
+                            </ul>
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item me-4">
+                                    <Link className="nav-link" to="/login">Login</Link>
+                                </li>
+                                {/* <li className="nav-item me-4">
+                    <Link className="nav-link" to="/register">Register</Link>
+                </li> */}
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
     );
 }
 
