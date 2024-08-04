506b91e824c7d5efd3841d780caae1c0dde94ba2
Pranav Chaudhari <whitespider080067@gmail.com>
2024-08-04 14:45:30 +0530

    Created server

---
+++ immersive-homes/backend/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const port = 4000;
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
