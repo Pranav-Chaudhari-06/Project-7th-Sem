aef1738908facb0a355fd4598c08aa10529d08b3
Pranav Chaudhari <whitespider080067@gmail.com>
2024-08-05 21:18:04 +0530

    Updated navbar (nothing major, ignore this)

--- immersive-homes/frontend/src/Navbar.js
+++ immersive-homes/frontend/src/Navbar.js
@@ -28,9 +28,6 @@ function Navbar() {
                                 <li className="nav-item me-4">
                                     <Link className="nav-link" to="/login">Login</Link>
                                 </li>
-                                {/* <li className="nav-item me-4">
-                                    <Link className="nav-link" to="/register">Register</Link>
-                                </li> */}
                             </ul>
                         </div>
                     </div>
